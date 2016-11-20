ec1b1b01e8e448ccb988d652dafdc48a6c3aa4a8
dmytro <dvinokurov777@gmail.com>
2016-11-20 22:56:29 +0100

    updated Runner

--- src/main/java/com/popova/Runner.java
+++ src/main/java/com/popova/Runner.java
@@ -1,22 +1,12 @@
 package com.popova;
 
-import com.sun.corba.se.spi.activation.Repository;
-
-import java.util.Date;
-
 public class Runner {
 
     public static void main(String[] args) {
         EventRepository repository = new EventRepositoryImpl();
-        Event event = getEvent();
-        repository.saveEvent(event);
+        EventService eventService = new EventServiceImpl(repository);
+        eventService.generateFakeDataSet();
+
     }
 
-    private static Event getEvent() {
-        Event event = new Event();
-        event.setEventType(Event.EventType.ACTIVITY);
-        event.setUserId(1);
-        event.setTimeStamp(new Date());
-        return event;
-    }
 }
